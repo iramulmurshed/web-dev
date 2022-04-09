a91f94006543f592eac8e762dc2624e4daa6a7cd
S.M.IRAMUL MURSHED <35691070+iramulmurshed@users.noreply.github.com>
2022-04-10 04:30:17 +0600

    Update script.js

--- DOM/script.js
+++ DOM/script.js
@@ -2,13 +2,26 @@ var button=document.getElementById("add");
 var input=document.getElementById("userinput");
 var ul=document.querySelector("ul");
 
-
-
-
-
 button.addEventListener("click",function(){
-    console.log("works");
+
+   if(input.value.length>0){
     var li=document.createElement("li");
-    li.appendChild(document.createTextNode(input.value))
+    li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
+    input.value="";
+   }
+  
 })
+
+input.addEventListener("keypress",function(event){
+
+    
+
+    if(input.value.length>0 && event.which===13){
+     var li=document.createElement("li");
+     li.appendChild(document.createTextNode(input.value));
+     ul.appendChild(li);
+     input.value="";
+    }
+   
+ })
